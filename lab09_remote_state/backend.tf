3ff6956c01312b07da52340284f8f17edac83c1f
Leandro Fabian Zenteno Soliz <leandrozentenosoliz16@gmail.com>
2026-05-21 17:09:05 -0400

    change backend

--- lab09_remote_state/backend.tf
+++ lab09_remote_state/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "my-bucket-state-lean23"
     key            = "lab09_lean/terraform.tfstate" # <-- path in bucket 
     region         = "us-east-2"
-    dynamodb_table = "terraform-lock"
+    use_lockfile   = true
   }
 }
